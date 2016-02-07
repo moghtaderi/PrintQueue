2ceeec39a98d6e38195cbfd8df63f94513fd0624
Reza Moghtaderi <rmoghtaderi@diablo.highpoint.edu>
2016-02-07 13:43:45 -0500

    added file IO

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include "headerfile.h"
 #include <time.h>
 #include <cstdlib>
-
+#include <fstream>
+#include <string>
 
 using namespace std;
 
@@ -13,6 +14,11 @@ int main(int argc, char const *argv[]) {
 	int maxPages;
 	char userSeed;
 	int seedValue;
+	char userOutput;
+	string fileName;
+	bool fileIO;
+	ofstream outfile;
+	streambuf* coutBuffer;
 
 	cout << endl << "+++ Welcome to the printer simulation! +++" << endl << endl;
 
@@ -36,8 +42,30 @@ int main(int argc, char const *argv[]) {
 	}
 	srand(seedValue);
 
+	cout << "Would you like the output on the screen [Y/N]: ";
+	cin >> userOutput;
+	if (userOutput == 'y' || userOutput == 'Y') {
+		fileIO = false;
+	}else{
+		cout << "Please enter the name of your output file: ";
+		cin >> fileName;                    // get output file name
+    	outfile.open(fileName.c_str());     // open file for write
+   	
+   	if(outfile.fail()) {
+      	cerr << "ERROR: could not create file <" << fileName << ">" << endl;
+      	exit(1);
+   	}
+
+   	fileIO = true;
+   	coutBuffer = cout.rdbuf();          // keep a backup of cout buffer
+   	cout.rdbuf(outfile.rdbuf());        // redirect cout to output file
+   	// NOTE: the rest of the file can use cout but it will be redirected to outfile
+	}
 
 	cout << printerCount << "-" << printerSpeed << "-" << numPrintJobs << "-" << maxPages << "-" << seedValue << endl;
+
+	cout.rdbuf(coutBuffer);                 // reset cout buffer
+	outfile.close();                        // close the output file buffer
 
 	return 0;
 }
